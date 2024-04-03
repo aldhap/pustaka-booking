244a8dda7ecb0cc1e44f71ea4679cadd83a8b57c
aldhap <aldhapebriyani214@gmail.com>
2024-04-03 11:52:47 +0700

    wuuubaboooo

---
+++ application/views/view-latihan.php
@@ -0,0 +1,10 @@
+<html>
+    
+<body>
+    Halo Kawan.. Yuk kita belajar web programing..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode perjumlahan yaitu <?=
+    $nilai1.'+' . $nilai2. '+' . $hasil; ?>
+</body>
+</html>
